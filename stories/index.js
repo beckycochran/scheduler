ba6808b963908bef25fe3148beedece5b78e23ed
Rebekah <rebekahmparkes@outlook.com>
2021-09-02 12:40:08 -0400

    Confirm Component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import DayList from 'src/components/DayList';
 import Appointment from 'src/components/Appointment/index';
 import Header from 'src/components/Appointment/Header';
 import Empty from 'src/components/Appointment/Empty';
+import Show from 'src/components/Appointment/Show';
+import Confirm from 'src/components/Appointment/Confirm';
+
+
 
 
 
@@ -108,7 +112,12 @@ storiesOf("Button", module)
       .add("Appointment", () => <Appointment />)
       .add("Appointment with Time", () => <Appointment time="12pm" />)
       .add("Header", () => <Header time="12pm" />)
-      .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+      .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+      .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+      .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />);
+
+      
+      
 
     
     
