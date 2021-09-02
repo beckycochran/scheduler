4f280bd05b1a77bfbef4ba9e75e575681ff69185
Rebekah <rebekahmparkes@outlook.com>
2021-09-02 10:02:12 -0400

    dynamically allocate Interviewer attributes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,24 @@
 import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+
+  // const interviewerProfile = props.item.map(
+  //   (item) => {
+  //     return (
+  //       <InterviewerListItem
+  //         id={item.id}
+  //         name={item.name}
+  //         avatar={item.avatar}
+  //         selected={item.selected}
+  //         setInterviewer={item.setInterviewer}
+  //       />
+  //     )
+  //   }
+  // );
+
+  // return <ul>{interviewerProfile}</ul>
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,17 +8,26 @@
 
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classnames from "classnames";
 
 export default function InterviewerListItem(props) {
+  const interviewer = classnames("interviewers_item", {
+    "day-list__item--id": props.selected,
+    "day-list__item--name": props.name,
+    "day-list__item--avatar": props.avatar,
+    "day-list__item--selected": props.selected,
+  })
+
   return (
-    <li className="interviewers__item">
-      <img
+
+    < li className = {interviewer} onClick={props.setInterviewer}>
+      {/* <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
-    </li>
+  Sylvia Palmer */}
+    </li >
 
   )
 }
