bc592aeaa914eec893d647c2bf87f0827b70190e
Rebekah <rebekahmparkes@outlook.com>
2021-09-08 11:42:32 -0400

    proper application component setup, commented out old code and will revise as needed

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,77 +7,129 @@ import Appointment from "components/Appointment/index.js";
 import axios from "axios";
 
 //import helper functions to help with loops
+//destroy/save transitions
+
+
+// export default function Application(props) {
+//   const [state, setState] = useState({
+//     day: "Monday",
+//     days: [],
+//     appointments: {}
+//   });
+
+
+//   function bookInterview(id, interview) {
+//     return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+//       dispatch({
+//         type: SET_INTERVIEW,
+//         id,
+//         interview
+//       });
+//     });
+//   }
+//   function cancelInterview(id) {
+//     return axios.delete(`/api/appointments/${id}`).then(() => {
+//       dispatch({
+//         type: SET_INTERVIEW,
+//         id,
+//         interview: null
+//       });
+//     });
+//   }
+
+//   const interviewers = getInterviewersForDay(state, state.day);
+//   function bookInterview(id, interview) {
+//     console.log(id, interview);
+//   }
+  
+//   function save(name, interviewer) {
+//     const interview = {
+//       student: name,
+//       interviewer
+//     };
+//   }
+  
+
+//   const setDay = day => dispatch({ type: "SET_DAY", value: day });
+//   useEffect(() => {
+//       Promise.all([
+//         axios.get("/api/days"),
+//         axios.get("/api/appointments"),
+//         axios.get("/api/interviewers"),
+//       ]).then((all) => {
+//         dispatch({
+//           type: SET_APPLICATION_DATA,
+//           value: {
+//             days: all[0].data,
+//             appointments: all[1].data,
+//             interviewers: all[2].data
+//           }
+//         });
+//       });
+//     }, []);
+  
+
+
+//   return (
+//     <main className="layout">
+//       <section className="sidebar">
+//         <img
+//           className="sidebar--centered"
+//           src="images/logo.png"
+//           alt="Interview Scheduler"
+//         />
+//         <hr className="sidebar__separator sidebar--centered" />
+
+//         <nav className="sidebar__menu">
+//           <DayList
+//             days={state.days}
+//             day={state.day}
+//             setDay={setDay}
+//           />
+//         </nav>
+
+//         <img
+//           className="sidebar__lhl sidebar--centered"
+//           src="images/lhl.png"
+//           alt="Lighthouse Labs"
+//         />
+
+//       </section>
+//       <section className="schedule">
+//         {list}
+//         <Appointment
+//           id='last'
+//           time="5pm"
+//         />
+//       </section>
+//     </main>
+//   );
+// }
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  });
-
-
-  function bookInterview(id, interview) {
-    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-      dispatch({
-        type: SET_INTERVIEW,
-        id,
-        interview
-      });
-    });
-  }
-  function cancelInterview(id) {
-    return axios.delete(`/api/appointments/${id}`).then(() => {
-      dispatch({
-        type: SET_INTERVIEW,
-        id,
-        interview: null
-      });
-    });
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const interviewers = getInterviewersForDay(state, state.day);
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-  }
-  
-  function save(name, interviewer) {
-    const interview = {
-      student: name,
-      interviewer
-    };
-  }
-  
 
-  const setDay = day => dispatch({ type: "SET_DAY", value: day });
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days/"),
-      axios.get("/api/appointments/"),
-      axios.get("/api/interviewers/")
-    ])
-      .then(url => {
-        dispatch({
-          type: "SET_APPLICATION_DATA",
-          days: url[0].data,
-          appointments: url[1].data,
-          interviewers: url[2].data
-        });
-      })
-      .catch(err => err);
-      const webSocket = new WebSocket("wss:https://interviewscheduler.netlify.com/");
-      webSocket.onopen = function() {
-        webSocket.send(`Socket initialized`);
-      };
-  
-      webSocket.onmessage = function(message) {
-        let parsedMessage = JSON.parse(message.data);
-        if (parsedMessage.type === "SET_INTERVIEW") {
-          dispatch({ ...parsedMessage });
-        }
-      };
-    }, []);
-
-
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={getInterview(state, appointment.interview)}
+          interviewers={interviewers}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
+    }
+  );
 
   return (
     <main className="layout">
@@ -88,29 +140,22 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-
         <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            day={state.day}
-            setDay={setDay}
-          />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
-
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
       </section>
       <section className="schedule">
-        {list}
-        <Appointment
-          id='last'
-          time="5pm"
-        />
+        <section className="schedule">
+          {appointments}
+          <Appointment key="last" time="5pm" />
+        </section>
       </section>
     </main>
   );
 }
+
