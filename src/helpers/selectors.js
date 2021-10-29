6729d868b011a657f8ae24ed0224a6afc2b832b5
Rebekah <rebekahmparkes@outlook.com>
2021-10-29 06:34:16 -0400

    replacing selector functions with scheduler 2's logic

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,32 +1,49 @@
-export function getAppointmentsForDay(state, name) {
-  let filteredDays = state.days.filter(day => {
-    return day.name === name;
-  });
-  if (filteredDays.length === 0) return [];
-  return filteredDays[0].appointments.map(
-    appointment => state.appointments[appointment]
-  );
-}
-
-export function getInterviewersForDay(state, name) {
-  let filteredDays = state.days.filter(day => {
-    return day.name === name;
-  });
-
-  if (filteredDays.length === 0) return [];
-
-  return filteredDays[0].interviewers.map(interviewer => {
-    return state.interviewers[interviewer];
-  });
+export function getAppointmentsForDay(state, day) {
+  let appointment_state = [];
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      appointment_state = [...state.days[i].appointments];
+    }
+  }
+  for (const appointment in state.appointments) {
+    for (let i = 0; i < appointment_state.length; i++) {
+      if (appointment_state[i] === state.appointments[appointment].id) {
+        appointment_state[i] = state.appointments[appointment];
+      }
+    }
+  }
+  return appointment_state;
 }
 
 
+export const getInterview = (state, interview) => {
+  if (!interview || !state.interviewers) {
+    return null;
+  }
+  let id = interview.interviewer;
+  let studentName = interview.student;
+  return {
+    interviewer:
+      state.interviewers[
+        String(Object.keys(state.interviewers).find(key => id === Number(key)))
+      ],
+    student: studentName
+  };
+};
 
-export function getInterview(state, interview) {
-  return !interview
-    ? null
-    : {
-        interviewer: state.interviewers[interview.interviewer],
-        student: interview.student
-      };
-}
+export const getInterviewersForDay = (state, day) => {
+  let ans = [];
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      ans = [...state.days[i].interviewers];
+    }
+  }
+  for (const interviewer in state.interviewers) {
+    for (let i = 0; i < ans.length; i++) {
+      if (ans[i] === Number(interviewer)) {
+        ans[i] = state.interviewers[String(interviewer)];
+      }
+    }
+  }
+  return ans;
+};
