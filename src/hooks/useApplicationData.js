35f0772f96d7e3efb1bb680e8285e30fbb418f28
Rebekah <rebekahmparkes@outlook.com>
2021-11-19 05:49:23 -0500

    import hook from scheduler 2 with added websocked functionality

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,71 +1,64 @@
 import { useEffect, useReducer } from "react";
-import axios from "axios";
 import reducer, {
   SET_DAY,
   SET_APPLICATION_DATA,
-  SET_INTERVIEW,
-} from "helpers/updateAppState";
+  SET_INTERVIEW
+} from "reducers/application";
+import axios from "axios";
+import {
+  getDayFromAppointmentId
+} from "../../helpers/selectors";
+require("dotenv").config();
 
+const ws = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-// Our useApplicationData Hook will return an object with four keys.
-
-//     The state object will maintain the same structure.
-//     The setDay action can be used to set the current day.
-//     The bookInterview action makes an HTTP request and updates the local state.
-//     The cancelInterview action makes an HTTP request and updates the local state.
-
-
-export default function useApplicationData() {
-
-
+export const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
-    appointments: {},
-    interviewers: {}
+    appointments: {}
   });
 
-  const setDay = day => dispatch({ type: SET_DAY, value: day });
+  const setDay = day => dispatch({ type: SET_DAY, day });
 
   useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
-    ]).then((all) => {
-      dispatch({
-        type: SET_APPLICATION_DATA,
-        value: {
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data
-        }
-      });
-    });
+    const getDays = axios.get("/api/days");
+    const getAppointments = axios.get("/api/appointments");
+    const getInterviewers = axios.get("/api/interviewers");
+    Promise.all([getDays, getAppointments, getInterviewers])
+      .then(res => {
+        dispatch({
+          type: SET_APPLICATION_DATA,
+          days: res[0].data,
+          appointments: res[1].data,
+          interviewers: res[2].data
+        });
+      })
+      .catch(err => console.log(err));
   }, []);
 
-  function bookInterview(id, interview, day) {
+  ws.onmessage = function(event) {
+    const { type, id, interview } = JSON.parse(event.data);
+    const day = getDayFromAppointmentId(state, id);
+    dispatch({ type, id, interview, day: day });
+  };
+
+  const bookInterview = (id, interview, day) => {
     return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-      dispatch({
-        type: SET_INTERVIEW,
-        id,
-        interview,
-        day
-      });
+      dispatch({ type: SET_INTERVIEW, id, interview, day });
     });
-  }
+  };
 
-  function cancelInterview(id, day) {
+  const cancelInterview = (id, day) => {
     return axios.delete(`/api/appointments/${id}`).then(() => {
-      dispatch({
-        type: SET_INTERVIEW,
-        id,
-        interview: null,
-        day
-      });
+      dispatch({ type: SET_INTERVIEW, id, interview: null, day });
     });
-  }
+  };
 
-  return { state, setDay, cancelInterview, bookInterview }
-
-}
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  };
+};
